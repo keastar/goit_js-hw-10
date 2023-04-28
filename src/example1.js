470c6efefb2b8c32545578b8e68f5de9df97398f
keastar <keastar1986@gmail.com>
2023-04-28 14:26:11 +0300

    example_filtered_result
    example_filtered_result

---
+++ src/example1.js
@@ -0,0 +1,49 @@
+import './css/styles.css';
+
+const languages = [
+  { label: 'HTML' },
+  { label: 'CSS' },
+  { label: 'JavaScript' },
+  { label: 'Node.js' },
+  { label: 'React' },
+  { label: 'Vue' },
+  { label: 'JAVA' },
+  { label: 'Pyton' },
+];
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  ulEl: document.querySelector('.country-list'),
+  divEl: document.querySelector('.country-info'),
+};
+refs.inputEl.addEventListener('input', onInputChange);
+
+const listItemsMarkup = createListItems(languages);
+
+// console.log(listItemsMarkup);
+populateListMarkup(listItemsMarkup);
+
+function createListItems(items) {
+  return items.map(item => `<li>${item.label}</li>`).join('');
+}
+
+function onInputChange(e) {
+  //из интерфейса получаем фильтр - конкретные буквы при вводе в поле input
+  const filter = e.target.value.toLowerCase();
+
+  //из модели const languages фильтруем только те объекты,
+  //   которые под этот фильтр подходят - содержать в названии определенную букву или сочитание букв
+  const filteredItems = languages.filter(l =>
+    l.label.toLowerCase().includes(filter)
+  );
+
+  //создаем разметку под отфильтрованный массив
+  const listItemsMarkup = createListItems(filteredItems);
+  // console.log(listItemsMarkup);
+  // записываем в список в интерфейсе отфильтрованную разметку
+  populateListMarkup(listItemsMarkup);
+}
+
+function populateListMarkup(markup) {
+  refs.ulEl.innerHTML = markup;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import './css/styles.css';
+// import throttle from 'lodash.throttle';
 
-const DEBOUNCE_DELAY = 300;
+// import './css/styles.css';
+
+// // Напиши функцию fetchCountries(name)
+// // которая делает HTTP - запрос на ресурс name
+// // и возвращает промис с массивом стран - результатом запроса
+
+// const refs = {
+//   inputEl: document.querySelector('#search-box'),
+//   ulEl: document.querySelector('.country-list'),
+//   divEl: document.querySelector('.country-info'),
+// };
+
+// const DEBOUNCE_DELAY = 300;
+
+// refs.inputEl.addEventListener('input', throttle(onSearch, DEBOUNCE_DELAY));
+
+// function onSearch(event) {
+//   event.preventDefault;
+
+//   const searchQuery = event.target.value;
+//   console.log(searchQuery);
+
+//   function fetchCountries(name) {
+//     // let name = searchQuery;
+//     const options = {};
+//     const url = `https://restcountries.com/v3.1/name/{name}`;
+//     fetch(url, options)
+//       .then(r => r.json())
+//       .then(console.log);
+//   }
+// }
